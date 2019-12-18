a3cb43321c21575be5373e1978fb06999db3aacc
ryan yen <45389924+ryanyen2@users.noreply.github.com>
2019-12-18 11:06:38 +0800

    Update Puzzle (Word_finals 1993, UVa227).cpp

--- Puzzle (Word_finals 1993, UVa227).cpp
+++ Puzzle (Word_finals 1993, UVa227).cpp
@@ -66,3 +66,106 @@ int main() {
 	return 0;
 }
 
+
+
+
+
+
+
+
+
+
+
+// Another solution
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cassert>
+using namespace std;
+
+#define _for(i, a, b) for (int i = (a); i<(b); ++i)
+#define _rep(i, a, b) for (int i = (a); i <= (b); i++)
+
+template<typename T>
+ostream& operator<<(ostream& os, const vector<T>& v) {
+	_for(i, 0, v.size()) os << v[i] << " ";
+	return os;
+}
+
+struct Point {
+	int x, y;
+	Point(int x = 0, int y = 0) : x(x), y(y) {}
+};
+typedef Point Vector;
+
+const int GSize = 5;
+vector<string> grid;
+Point ePos;
+map<char, Vector> DIRS;
+
+bool valid(const Point& p) {
+	return p.x >= 0 && p.x < GSize && p.y >= 0 && p.y < GSize;
+}
+
+void print_grid() {
+	_for(i, 0, GSize) {
+		_for(j, 0, GSize) {
+			if (j) cout << ' ';
+			cout << grid[i][j];
+		}
+		cout << endl;
+	}
+}
+
+bool try_move(char cmd) {
+	if (!DIRS.count(cmd)) return false;
+	Point p = ePos + DIRS[cmd];
+	if (!valid(p)) return false;
+	swap(grid[p.x][p.y], grid[ePos.x][ePos.y]);
+	ePos = p;
+	return true;
+}
+
+
+int main() {
+
+	int t = 1;
+	string line;
+	DIRS['A'] = Vector(-1, 0); DIRS['B'] = Vector(1, 0); DIRS['L'] = Vector(0, -1); DIRS['R'] = Vector(0, 1);
+	while (true) {
+		grid.clear();
+		ePos.x = -1;
+		ePos.y = -1;
+		_for(i, 0, GSize) {
+			getline(cin, line);
+			if (line == "Z") return 0;
+			assert(line.size() == GSize);
+			_for(j, 0, GSize) {
+				if (line[j] == ' ') {
+					assert(ePos.x == -1 && ePos.y == -1);
+					ePos.x = i;
+					ePos.y = j;
+				}
+				grid.push_back(line);
+			}
+		}
+		char move;
+		string moves;
+		while (true) {
+			getline(cin, moves);
+			assert(!line.empty());
+			bool end = *(line.rbegin()) == '0';
+			if (!end) moves.append(line);
+			else moves.append(line, 0, line.size() - 1);
+			if (end) break;
+		}
+		bool legal = true;
+		_for(i, 0, moves.size())
+			if (!try_move(moves[i])) { legal = false; break; }
+		if (t > 1) cout << endl;
+		cout << "Puzzle #" << t++ << " : " << endl;
+		if (legal) print_grid();
+		else cout << "This puzzle has no final configuration." << endl;
+	}
+	return 0;
+}
