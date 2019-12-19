bacc5c5324240fe8a3a0573006d7e4ab6b336524
ryan yen <45389924+ryanyen2@users.noreply.github.com>
2019-12-19 11:08:52 +0800

    Update Crossword Answers (Word_finals 1994, UVa232).cpp

--- Crossword Answers (Word_finals 1994, UVa232).cpp
+++ Crossword Answers (Word_finals 1994, UVa232).cpp
@@ -72,3 +72,86 @@ int main()
     }
     return 0;
 }
+
+
+
+
+// Another answer
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cassert>
+using namespace std;
+
+#define _for(i, a, b) for (int i = (a); i<(b); ++i)
+#define _rep(i, a, b) for (int i = (a); i <= (b); i++)
+
+template<typename T>
+ostream& operator<<(ostream& os, const vector<T>& v) {
+	_for(i, 0, v.size()) os << v[i] << " ";
+	return os;
+}
+
+struct Point {
+	int x, y;
+	Point(int x = 0, int y = 0):x(x), y(y){}
+};
+typedef Point Vector;
+
+Vector operator+(const Vector& A, const Vector& B) { return Vector(A.x + B.x, A.y + B.y); }
+int row, col;
+const int MAXC = 16;
+char grid[MAXC][MAXC];
+
+inline bool valid(const Point& p) {
+	return p.x >= 0 && p.x < row && p.y >= 0 && p.y < col;
+}
+int main() {
+
+	char buf[MAXC]; int buf_len;
+	const Vector dLeft(0, -1), dRight(0, 1), dUp(-1, 0), dDown(1, 0);
+	for (int t = 1; cin >> row && (row); t++) {
+		cin >> col;
+		vector <Point> eligible;
+		vector <int> down, accross;
+
+		if (t > 1) cout << " ";
+		printf("Puzzle #%d:\n", t);
+		_for(i, 0, row) {
+			cin >>  grid[i];
+			_for(j, 0, col) {
+				if (grid[i][j] == '*') continue;
+				Point p(i, j), left = p + dLeft, up = p + dUp;
+				bool isCross = !valid(left) || grid[left.x][left.y] == '*';
+				bool isDown = !valid(up) || grid[up.x][up.y] == '*';
+				if (isCross) accross.push_back(eligible.size());
+				if (isDown) down.push_back(eligible.size());
+				if (isDown || isCross) eligible.push_back(p);
+			}
+		}
+		puts("Accross");
+		for (auto n : accross) {
+			buf_len = 0, memset(buf, 0, sizeof(buf));
+			Point p = eligible[n];
+			while (valid(p) && grid[p.x][p.y] != '*') {
+				buf[buf_len++] = grid[p.x][p.y];
+				p = p + dRight;
+			}
+			printf("%3d.%s\n", n + 1, buf);
+		}
+		puts("Down");
+		for (auto m : down) {
+			buf_len = 0, memset(buf, 0, sizeof(buf));
+			Point p = eligible[m];
+			while (valid(p) && grid[p.x][p.y] != '*') {
+				buf[buf_len++] = grid[p.x][p.y];
+				p = p + dDown;
+			}
+			printf("%3d.%s\n", m + 1, buf);
+		}
+	}
+	
+	return 0;
+}
+
